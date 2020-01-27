ad2b3339f5d377366718448ec01029a36da5a82d
finnam <andy_finn@btinternet.com>
2020-01-27 02:42:17 -0800

    Added Tests

--- Controllers/CleanseController.cs
+++ Controllers/CleanseController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.JsonPatch;
@@ -19,7 +20,10 @@ namespace DataCleaner.Controllers
         const ushort RETRIES = 3;
 
         const long DELAY_TICKS = 10000;
-        private readonly ILogger<DataCleanerController> _logger;
+
+        readonly Regex validator = new Regex("^[0-9]{4}$");
+ 
+        readonly ILogger<DataCleanerController> _logger;
 
         public DataCleanerController(ILogger<DataCleanerController> logger)
         {
@@ -27,49 +31,60 @@ namespace DataCleaner.Controllers
         }
 
         [HttpPost("cleanse")]
-        public async Task<IActionResult> Post([FromBody] ExpandoObject p)
+        public async Task<IActionResult> Post([FromBody] ExpandoObject patient)
         {       
             try
-            {  
-                var id = ((dynamic) p).id.ToString();
-                _logger.LogInformation($"cleanse: called for patient {id}.  ");
-
-                var innerApi = new Utils.RestApi(BASE_URL);
-                Func<Task<string>> post = async () => await innerApi.GetEndPointAsync("preferences", id);
-
-                var prefs = await Retry(post, new TimeSpan(DELAY_TICKS), RETRIES); 
-                
-                if ( !String.IsNullOrEmpty(prefs))
+            {   
+                object v  = null;
+                if(((IDictionary<String, object>)patient).TryGetValue("id", out v) && validator.IsMatch(v.ToString()) )
                 {
-                    var jDoc = JsonDocument.Parse(prefs);
-                    var pref = jDoc.RootElement.GetProperty("patientPreference");
-                    if( pref.GetString().Equals("OBFUSCATE_ID"))
-                    {
-                       _logger.LogInformation($"cleanse: patient {id} obfuscated.  ");
-                        var newId = jDoc.RootElement.GetProperty("newId");
-                        JsonPatchDocument patch = new JsonPatchDocument();
-                        patch.Replace("/id", newId.GetString());
+                         
+                    var id = ((dynamic) patient).id.ToString();
 
-                        patch.ApplyTo(p);
+                    _logger.LogInformation($"cleanse: called for patient {id}.  ");
+
+                    var innerApi = new Utils.RestApi(BASE_URL);
+                    Func<Task<string>> post = async () => await innerApi.GetEndPointAsync("preferences", id);
+
+                    var prefs = await Retry(post, new TimeSpan(DELAY_TICKS), RETRIES); 
+                    
+                    if ( !String.IsNullOrEmpty(prefs))
+                    {
+                        var jDoc = JsonDocument.Parse(prefs);
+                        var pref = jDoc.RootElement.GetProperty("patientPreference");
+                        if( pref.GetString().Equals("OBFUSCATE_ID"))
+                        {
+                        _logger.LogInformation($"cleanse: patient {id} obfuscated.  ");
+                            var newId = jDoc.RootElement.GetProperty("newId");
+                            JsonPatchDocument patch = new JsonPatchDocument();
+                            patch.Replace("/id", newId.GetString());
+
+                            patch.ApplyTo(patient);
+                        }
+                        else
+                        {
+                        _logger.LogInformation($"cleanse: patient {id} clear text.  ");
+                        }
                     }
                     else
                     {
-                       _logger.LogInformation($"cleanse: patient {id} clear text.  ");
+                        _logger.LogError($"cleanse: Gateway Timeout after {RETRIES} attempts.  ");
+                        return StatusCode(StatusCodes.Status504GatewayTimeout);
                     }
                 }
                 else
                 {
-                    _logger.LogError($"cleanse: Gateway Timeout after {RETRIES} attempts.  ");
-                    return StatusCode(StatusCodes.Status504GatewayTimeout);
+                    // Bad or missing Id
+                    return StatusCode(StatusCodes.Status400BadRequest);
                 }
             }
             catch(Exception ex)
             {
                 _logger.LogCritical($"cleanse: Internal Server Error {ex.Message}.  ");
-                 StatusCode(StatusCodes.Status500InternalServerError);
+                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
-            return ( new JsonResult(p));
+            return ( new JsonResult(patient));
         }  
 
         public async Task<T> Retry<T>(Func<Task<T>> action, TimeSpan retryInterval, int retryCount) 
--- Makefile
+++ Makefile
@@ -8,6 +8,21 @@ serve:
 	docker start data-cleaner-container
 
 test: 
-	curl -k --header "Content-Type: application/json" -d '{"row-id": 1, "id": "0001", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse
-	curl -k --header "Content-Type: application/json" -d '{"row-id": 1, "id": "0002", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse
-	curl -k --header "Content-Type: application/json" -d '{"row-id": 1, "id": "0003", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse
+	@echo "\nTest Id 0001 is Obfuscated"
+	@curl -k --header "Content-Type: application/json" -d '{"row-id": 1, "id": "0001", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse
+	@echo "\nTest Id 0002 is Not Obfuscated"
+	@curl -k --header "Content-Type: application/json" -d '{"row-id": 1, "id": "0002", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse
+	@echo "\nTest correct response on inner api fail"
+	@if curl -k -s -o /dev/null -w "%{http_code}" --header "Content-Type: application/json" -d '{"row-id": 1, "id": "0003", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse | grep 504; then echo "pass"; else echo "fail"; fi
+	@echo "\nTest validation id not string. "
+	@if curl -k -s -o /dev/null -w "%{http_code}" --header "Content-Type: application/json" -d '{"row-id": 1, "id": 1, "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse | grep 400; then echo "pass"; else echo "fail"; fi
+	@echo "\nTest validation id to short. "
+	@if curl -k -s -o /dev/null -w "%{http_code}" --header "Content-Type: application/json" -d '{"row-id": 1, "id": "1", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse | grep 400; then echo "pass"; else echo "fail"; fi
+	@echo "\nTest validation id to long. "
+	@if curl -k -s -o /dev/null -w "%{http_code}" --header "Content-Type: application/json" -d '{"row-id": 1, "id": "10000", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse | grep 400; then echo "pass"; else echo "fail"; fi
+	@echo "\nTest validation id to lower limit. "
+	@if curl -k -s -o /dev/null -w "%{http_code}" --header "Content-Type: application/json" -d '{"row-id": 1, "id": "0001", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse | grep 200; then echo "pass"; else echo "fail"; fi
+	@echo "\nTest validation id to upper limit. "
+	@if curl -k -s -o /dev/null -w "%{http_code}" --header "Content-Type: application/json" -d '{"row-id": 1, "id": "9999", "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse | grep 200; then echo "pass"; else echo "fail"; fi
+	@echo "\nTest validation id missing. "
+	@if curl -k -s -o /dev/null -w "%{http_code}" --header "Content-Type: application/json" -d '{"row-id": 1, "fred": "fred"}' -X POST http://localhost:8080/datacleaner/cleanse | grep 400; then echo "pass"; else echo "fail"; fi
